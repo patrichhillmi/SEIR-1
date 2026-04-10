ae5fe260661465bead1d7bf271d480b803ef2bc8
BalericaAI <balericaai@gmail.com>
2026-04-10 16:57:47 -0500

    Create 6-firewall.tf

---
+++ weekly_lessons/weekb/terraform/6-firewall.tf
@@ -0,0 +1,31 @@
+resource "google_compute_firewall" "allow_ssh" {
+  name    = "allow-ssh"
+  network = google_compute_network.main.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"] # Lab only
+
+  depends_on = [
+    google_compute_network.main
+  ]
+}
+
+resource "google_compute_firewall" "allow_http" {
+  name    = "allow-http"
+  network = google_compute_network.main.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+
+  depends_on = [
+    google_compute_network.main
+  ]
+}
