9e6c7698252de7524012deb34918785e140e09ad
BalericaAI <balericaai@gmail.com>
2026-04-11 21:46:10 -0500

    Update main.tf

--- weekly_lessons/weekc/terraform/lab4_modules/waf/main.tf
+++ weekly_lessons/weekc/terraform/lab4_modules/waf/main.tf
@@ -62,3 +62,49 @@ resource "google_compute_backend_service" "secured_backend" {
 }
 
 
+
+variable "name" {
+  type = string
+}
+
+variable "enable_geo_blocking" {
+  type    = bool
+  default = true
+}
+
+variable "blocked_region_codes" {
+  type    = list(string)
+  default = []
+}
+
+variable "enable_rate_limit" {
+  type    = bool
+  default = true
+}
+
+variable "rate_limit_count" {
+  type    = number
+  default = 100
+}
+
+variable "rate_limit_interval_sec" {
+  type    = number
+  default = 60
+}
+
+variable "enable_waf_rules" {
+  type    = bool
+  default = true
+}
+
+variable "enable_bot_management" {
+  type    = bool
+  default = false
+}
+
+variable "recaptcha_redirect_site_key" {
+  type    = string
+  default = ""
+}
+
+
